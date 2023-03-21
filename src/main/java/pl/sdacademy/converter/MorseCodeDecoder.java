4464d19046d05ee114efb3448a1bd3cebe2b353f
etylinek <122802525+etylinek@users.noreply.github.com>
2023-03-21 19:51:59 +0100

    task3 - decoder and encoder added

---
+++ src/main/java/pl/sdacademy/converter/MorseCodeDecoder.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.converter;
+
+public class MorseCodeDecoder {
+
+    public String decode(String morseCode){
+        return "";
+    }
+}
---
+++ src/main/java/pl/sdacademy/converter/MorseCodeEncoder.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.converter;
+
+public class MorseCodeEncoder {
+
+    public String encode(String text){
+        return "";
+    }
+}
